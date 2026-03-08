c12dea87345d2ee9cbc99b3b8a037710c305032e
RyosukeSomeya <ryosuke.someya.0618.dev@gmail.com>
2026-03-08 04:32:57 +0000

    update

--- lambda/get_all_weather_public/index.py
+++ lambda/get_all_weather_public/index.py
@@ -4,6 +4,7 @@ import boto3
 dynamodb_client = boto3.client('dynamodb')
 
 def lambda_handler(event, context):
+    pring("deployed codebuild")
     response = dynamodb_client.scan(
         TableName='simple-weather-news-table',
     )
